04bfd7c871aae5faa3201357bfc96c5170c93719
PeteGabriel <pedrogabriel@protonmail.ch>
2025-04-04 19:09:54 +0200

    add an example of method documentation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rust-wannabe
+
+
+
+### Command-line utilities
+
+* echo: repeats input
+* cat: concatenates files
+* ls: lists directories
+* find: locates files or directories
+* grep: matches text in files
--- with_cargo/utilities/src/echo.rs
+++ with_cargo/utilities/src/echo.rs
@@ -1,5 +1,14 @@
 
-
+/// Echo repeats input.
+///
+/// # Examples
+///
+/// ```
+/// <read "Hello" input from cmd line>
+/// echo::echo(to_echo);
+///
+/// <prints "Hello">
+/// ```
 pub fn echo(txt: String){
   println!("{txt}");
 }
